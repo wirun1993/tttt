3e1f54c791c3f7cd559fd126773c932a820cd1aa
wirunkhomsai <wirunkhomsai1993@gmail.com>
2025-01-02 19:50:09 +0700

    1

---
+++ dist/_nuxt/static/1735819222/app/home/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdeploymovie\u002F_nuxt\u002Fstatic\u002F1735819222",layout:"layouts",error:a,serverRendered:true,routePath:"\u002Fapp\u002Fhome",config:{_app:{basePath:"\u002Fdeploymovie\u002F",assetsPath:"\u002Fdeploymovie\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1735819222/app/pages/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/app/pages", {data:[{}],fetch:{},mutations:void 0});
